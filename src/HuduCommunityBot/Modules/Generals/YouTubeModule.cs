ac13a24c79a29f3124dd41458f63cd59a48e778d
Mikey O'Toole <mikey@homotechsual.dev>
2026-06-14 19:22:17 +0100

    fix: update summary for set-default-template command to reference README for supported placeholders

--- src/HuduCommunityBot/Modules/Generals/YouTubeModule.cs
+++ src/HuduCommunityBot/Modules/Generals/YouTubeModule.cs
@@ -69,7 +69,7 @@ public class YouTubeModule : InteractionModuleBase<SocketInteractionContext>
     }
 
     [SlashCommand("set-default-template", "Set the default forum post title template")]
-    public async Task SetDefaultTemplateAsync([Summary("template", "Title template. Supports {ChannelName}, {VideoTitle}, {VideoId}, {VideoUrl}, {ChannelId}, {PublishedDate}")] string template)
+    public async Task SetDefaultTemplateAsync([Summary("template", "Title template; see README for supported placeholders")] string template)
     {
         await DeferAsync(ephemeral: true);
 
